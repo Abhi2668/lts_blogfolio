3f762e8b7993912ea74e90091a653e86592106bd
Abhinav Vinod <avinod34@gatech.edu>
2026-01-05 12:18:47 +0400

    another fix for date time??

--- app/year/YearPixelsClient.tsx
+++ app/year/YearPixelsClient.tsx
@@ -240,7 +240,8 @@ export default function YearPixelsClient({ initialLogs, currentYear }: YearPixel
   };
 
   function parseLocalDate(dateStr: string): Date {
-    return new Date(dateStr + 'T12:00:00-05:00');
+    const [year, month, day] = dateStr.split('-').map(Number);
+    return new Date(year, month - 1, day);
   }
 
   return (
